af26aaf54246cfc8073e74309f894850c8d9ccd4
zackynz <nurasfanz@gmail.com>
2024-06-14 14:17:40 +0700

    mengoperasikan while untuk data yang bersifat berulang

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -57,4 +57,16 @@ public:
 	}
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
+		//This function searches the currentnode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = nullptr;
+		while ((currentNode != nullptr) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
 	
