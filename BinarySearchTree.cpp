ef3a17f98e53099f73186dd31f9d1623dd874df2
zackynz <nurasfanz@gmail.com>
2024-06-14 14:19:32 +0700

    mengoperasikan if untuk menguji true or false pada deklarasi yang diberikan

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -71,4 +71,16 @@ public:
 	}
 	void inorder(Node* ptr)
 	{
-		
+		if (ROOT == nullptr)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != nullptr)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
+	
