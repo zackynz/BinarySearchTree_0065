fd2df012de7db5b19b40a5c3a01222762ac2fee2
zackynz <nurasfanz@gmail.com>
2024-06-14 14:11:56 +0700

    menambahkan struktur data baru pada element

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -31,4 +31,13 @@ public:
 
 	void insert(string element) //Insert a node in the binary search tree
 	{
+		Node* newNode = new Node(element, nullptr, nullptr); //Allocate memory for the new node
+		newNode->info = element; //assign value to the data field of the new node
+		newNode->leftchild = nullptr; //make the left child of the new node point to NULL
+		newNode->rightchild = nullptr; //make the right child of the new node point to NULL
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+		search(element, parent, currentNode); //Locate the node which will be the parent of the node to be insert
+
 		
