16379f4f704bc5f434d69902878ac81c068a7d4f
zackynz <nurasfanz@gmail.com>
2024-06-14 14:28:17 +0700

    mendeklarasikan interger sebagai data utama yang akan dibca pertama kali oleh sistem dan menampilkan data yang diperintahkan

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -110,3 +110,59 @@ public:
 };
 
 
+int main()
+{
+	BinaryTree x;
+	while (true)
+	{
+		cout << "\nMenu" << endl;
+		cout << "1. Impleement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. Perform preorder traversal" << endl;
+		cout << "4.  Perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			x.insert(word);
+			break;
+		}
+		case '2':
+		{
+			x.inorder(x.ROOT);
+			break;
+		}
+		case '3':
+		{
+			x.preorder(x.ROOT);
+			break;
+		}
+		case '4':
+		{
+			x.postorder(x.ROOT);
+			break;
+
+		}
+		case '5':
+		{
+			return 0;
+		}
+		default:
+		{
+			cout << "invalid choice.Try again." << endl;
+			break;
+		}
+		}
+	}
+	return 0;
+}
